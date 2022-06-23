ecffd8c36092724d8be85b7b1d53cb922183a067
mampudi <eichlefoko@gmail.com>
2022-06-23 20:42:12 +0200

    Added instructions to the readme file

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# mixasses
+# Assessment
+
+
+# Requirements
+
+* .NET Core 3.1
+
+## Installation
+
+1. Install .NET Core 3.1
+
+if you do not have .NET Core download and install in from below
+
+https://dotnet.microsoft.com/en-us/download/dotnet/3.1
+
+2. Clone this repository and cd into the folder:
+
+```bash
+git clone https://github.com/mampudi/mixasses.git
+```
+```bash
+cd mixasses/mixasses.console
+```
+```bash
+dotnet build
+```
+or
+
+Hit the build button in Visual Studio
+
+## Test
+Change into the test project directory
+```bash
+cd mixasses/mixasses.Integration
+```
+```bash
+dotnet test
+```
+or
+
+Run all tests in visual studio
+## Run the console application
+Change into the console project directory
+```bash
+cd mixasses/mixasses.console
+```
+```bash
+dotnet run
+```
+or
+
+Run all the console application from visual studio
--- mixasses/mixasses.console/Program.cs
+++ mixasses/mixasses.console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using mixasses.applicationservice.Model;
 using mixasses.applicationservice.Services;
 
@@ -8,9 +9,16 @@ namespace mixasses.console
     {
         static void Main(string[] args)
         {
-            var vehicleService = AddVehicleWithVehicleService();
+            var _vehicleService = AddVehicleWithVehicleService();
 
             //serialize to binary
+            var fileName = "vehiclesbinary.dat";
+            _vehicleService.Serialize(fileName);
+            Console.WriteLine(string.Format("The serialized file has been created with the name {0}", fileName));
+
+
+            var deserializedVehiclesFromBianry = _vehicleService.Deserialize(fileName);
+            Console.WriteLine(string.Format("The number of vehicles desirialized from binary is {0}", deserializedVehiclesFromBianry.Count()));
 
             Console.ReadLine();
         }
@@ -25,23 +33,101 @@ namespace mixasses.console
 
             //injecting is so that if there changes in future we can switch to json or xml serializer with minimum change here
             //The only responsibility of the vehicle service is to manage vehicles (SRP)
-            var vehicleService = new VehicleService(binarySerializer);
+            var _vehicleService = new VehicleService(binarySerializer);
 
-            //Show the number of vehicles when the program started
-            Console.WriteLine(string.Format("Number of vehicles when the program started {0}", vehicleService.NumberOfVehicles().ToString()));
-
-            vehicleService.AddVehicle(new Vehicle
+            _vehicleService.AddVehicle(new Vehicle
             {
                 PositionId = 1,
-                VehicleRegistration = "CV47WVGP",
-                Latitude = -25.98953,
-                Longitude = 28.12843,
+                VehicleRegistration = "aaaaaa",
+                Latitude = 34.544909,
+                Longitude = -102.100843,
                 RecordedTimeUTC = DateTime.UtcNow.Ticks
             });
 
-            //Show the number of vehicles after adding one vehicle
-            Console.WriteLine(string.Format("Number of vehicles after adding 1 vehicle is {0}", vehicleService.NumberOfVehicles().ToString()));
-            return vehicleService;
+            _vehicleService.AddVehicle(new Vehicle
+            {
+                PositionId = 2,
+                VehicleRegistration = "bbbbb",
+                Latitude = 32.345544,
+                Longitude = -99.123124,
+                RecordedTimeUTC = DateTime.UtcNow.Ticks
+            });
+
+            _vehicleService.AddVehicle(new Vehicle
+            {
+                PositionId = 3,
+                VehicleRegistration = "cccccc",
+                Latitude = 33.234235,
+                Longitude = -100.214124,
+                RecordedTimeUTC = DateTime.UtcNow.Ticks
+            });
+
+            _vehicleService.AddVehicle(new Vehicle
+            {
+                PositionId = 4,
+                VehicleRegistration = "ddddd",
+                Latitude = 35.195739,
+                Longitude = -95.348899,
+                RecordedTimeUTC = DateTime.UtcNow.Ticks
+            });
+
+            _vehicleService.AddVehicle(new Vehicle
+            {
+                PositionId = 5,
+                VehicleRegistration = "eeeee",
+                Latitude = 31.895839,
+                Longitude = -97.789573,
+                RecordedTimeUTC = DateTime.UtcNow.Ticks
+            });
+
+            _vehicleService.AddVehicle(new Vehicle
+            {
+                PositionId = 6,
+                VehicleRegistration = "fffff",
+                Latitude = 32.895839,
+                Longitude = -101.789573,
+                RecordedTimeUTC = DateTime.UtcNow.Ticks
+            });
+
+            _vehicleService.AddVehicle(new Vehicle
+            {
+                PositionId = 7,
+                VehicleRegistration = "ggggg",
+                Latitude = 34.115839,
+                Longitude = -100.225732,
+                RecordedTimeUTC = DateTime.UtcNow.Ticks
+            });
+
+            _vehicleService.AddVehicle(new Vehicle
+            {
+                PositionId = 8,
+                VehicleRegistration = "hhhhh",
+                Latitude = 32.335839,
+                Longitude = -99.992232,
+                RecordedTimeUTC = DateTime.UtcNow.Ticks
+            });
+
+            _vehicleService.AddVehicle(new Vehicle
+            {
+                PositionId = 9,
+                VehicleRegistration = "iiiii",
+                Latitude = 33.535339,
+                Longitude = -94.792232,
+                RecordedTimeUTC = DateTime.UtcNow.Ticks
+            });
+
+            _vehicleService.AddVehicle(new Vehicle
+            {
+                PositionId = 10,
+                VehicleRegistration = "jjjjj",
+                Latitude = 32.234235,
+                Longitude = -100.222222,
+                RecordedTimeUTC = DateTime.UtcNow.Ticks
+            });
+
+            Console.WriteLine(string.Format("The number of vehicles is {0}", _vehicleService.NumberOfVehicles().ToString()));
+
+            return _vehicleService;
         }
     }
 }
